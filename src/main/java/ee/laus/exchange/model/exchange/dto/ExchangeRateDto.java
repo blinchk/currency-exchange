2f29b82f65a4464a85f4cd2485d92c190761e7c7
Nikolas Laus <bchik3@gmail.com>
2024-05-26 22:43:22 +0300

    fix typo

--- src/main/java/ee/laus/exchange/model/exchange/dto/ExchangeRateDto.java
+++ src/main/java/ee/laus/exchange/model/exchange/dto/ExchangeRateDto.java
@@ -8,18 +8,18 @@ import java.time.LocalDate;
 
 import static ee.laus.exchange.util.CurrencyUtil.DEFAULT_CURRENCY;
 
-public record ExchangeRatioDto(
+public record ExchangeRateDto(
         String code,
         Double amount,
         LocalDate date
 ) {
 
-    public ExchangeRatioDto of(ExchangeRateApiResponse response) {
+    public ExchangeRateDto of(ExchangeRateApiResponse response) {
         CurrencyAmountApiResponse amount = response.amounts().stream()
                 .filter(responseAmount -> !responseAmount.code().equals(DEFAULT_CURRENCY))
                 .findFirst()
                 .orElseThrow(IllegalExchangeRateException::new);
-        return new ExchangeRatioDto(
+        return new ExchangeRateDto(
                 amount.code(),
                 amount.amount(),
                 response.date()
